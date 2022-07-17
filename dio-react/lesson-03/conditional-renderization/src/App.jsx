87e317a22a83863dc5ac5028d6f0d3223aed53a2
venerfruet <venerfruet@gmail.com>
2022-07-17 18:02:48 -0300

    aula3

---
+++ dio-react/lesson-03/conditional-renderization/src/App.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+const buttonA=<button>Histórico de Clientes</button>
+const buttonB=<button>Cadastrar Clientes</button>
+
+const hasCustomer=true
+
+const App=()=>{
+
+  const renderShowHistory=()=>(
+    <div>
+      Clique no botão abaixo para visualizar o histórico dos clientes
+      <br/>
+      {buttonA}
+    </div>
+  )
+
+  const renderAddCustomer=()=>(
+    <div>
+      Clique no botão abaixo para cadastrar o cliente
+      <br/>
+      {buttonB}
+    </div>
+  )
+
+  const showCustomer=()=>{
+    if(!hasCustomer) return null
+
+    return(
+      <div>
+        <h1>Nome do Cliente: Vener Fruet</h1>
+      </div>
+    )
+
+  }
+
+  return(
+    <div>
+      <p>Digital Innovation One</p>
+      <p>Bem vindo a nossa aula ;)</p>
+      {hasCustomer ? renderShowHistory() : renderAddCustomer()}
+      {showCustomer()}
+    </div>
+  );
+};
+
+export default App;
