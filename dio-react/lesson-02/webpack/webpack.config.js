d239058a9a17ea4ba4f582233398680a5c920591
venerfruet <venerfruet@gmail.com>
2022-07-17 14:16:53 -0300

    eslint

--- dio-react/lesson-02/webpack/webpack.config.js
+++ dio-react/lesson-02/webpack/webpack.config.js
@@ -2,6 +2,7 @@ const path=require('path')
 const HtmlWebPackPlugin=require("html-webpack-plugin")
 
 module.exports={
+  devtool: 'source-map',
   entry:'./src/index.js',
   output:{
     path:path.resolve(__dirname,'dist'),
