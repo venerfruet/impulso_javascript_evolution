914a8d025d55fd4070e2daf2e2becfd917433fa4
venerfruet <venerfruet@gmail.com>
2022-07-18 12:19:32 -0300

    revisão

---
+++ dio-react/lesson-03/thinking-react/src/containers/App.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import mock from '../mock'
+import Button from '../components/Button'
+
+const App=()=>{
+
+  const handleClick=(id)=>{
+    console.log('ID do Clinete:', id)
+  }
+
+  const renderCustomers=(customer)=>{
+    return(
+      <div key={`customer-${customer.id}`}>
+        <li>{customer.name} <Button onClick={(e=>handleClick(customer.id))}>X</Button></li>
+        {customer.skills.map(renderSkills)}
+      </div>
+    )
+  }
+  
+  const renderSkills=(skill, index)=>{
+    return(
+      <div style={{paddingLeft: '27px'}} key={`skill-${index}`}>
+        <li>{skill}</li>
+      </div>
+    )
+  }
+
+  return(
+    <div>
+      <p>Digital Innovation One</p>
+      <p>Bem vindo a nossa aula!</p>
+      <h1>Thinking React</h1>
+      <ul>
+        {mock.map(renderCustomers)}
+      </ul>
+    </div>
+  )
+}
+
+export default App
---
+++ dio-react/lesson-03/thinking-react/src/mock.js
@@ -0,0 +1,24 @@
+const mock=[
+  {
+    id:1,
+    name:'Vener Fruet da Silveira',
+    skills:['React', 'Node', 'CSS', 'Webpack']
+  },
+  {
+    id:2,
+    name:'Vener Fruet',
+    skills:['HTML', 'React Native', 'Go', 'JS']
+  },
+  {
+    id:3,
+    name:'Vener da Silveira',
+    skills:['Assembly']
+  },
+  {
+    id:4,
+    name:'Fruet daSilveira',
+    skills:['Reason']
+  },
+]
+
+export default mock
