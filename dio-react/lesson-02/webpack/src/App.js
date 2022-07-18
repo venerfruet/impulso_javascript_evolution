c75383293a2fc179d9a605bbb37cbbf27a351be9
venerfruet <venerfruet@gmail.com>
2022-07-18 10:05:00 -0300

    revisão

---
+++ dio-react/lesson-02/webpack/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Button from './Button'
+
+const App=()=>{
+  return(
+    <div>
+      <p>Digital Innovation One</p>
+      <p>Bem vindo a nossa aula!</p>
+      <p>Webpack 4</p>
+      <Button />
+    </div>
+  )
+}
+
+export default App
---
+++ dio-react/lesson-02/webpack/src/Button.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Button=()=>{
+  return(
+    <button>Olá : )</button>
+  )
+}
+
+export default Button
