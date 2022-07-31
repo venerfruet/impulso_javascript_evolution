92932079822d11945fa65ca2ff00b6598b70c69d
venerfruet <venerfruet@gmail.com>
2022-07-31 18:57:41 -0300

    Updates

---
+++ precache-manifest.dba093a37f245f5b87589203478595e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "65fa3bf9780192678696",
+    "url": "/perolas_homer/static/js/main.65fa3bf9.chunk.js"
+  },
+  {
+    "revision": "d3f6e060f2204756cf89",
+    "url": "/perolas_homer/static/js/1.d3f6e060.chunk.js"
+  },
+  {
+    "revision": "bec6f88525c76f9b50c6",
+    "url": "/perolas_homer/static/js/runtime~main.bec6f885.js"
+  },
+  {
+    "revision": "c9c955f32a0480398ea73a3cf17d7b88",
+    "url": "/perolas_homer/static/media/felix.c9c955f3.png"
+  },
+  {
+    "revision": "e2d2235eb01f1eeb084a40e20d65dd03",
+    "url": "/perolas_homer/static/media/logo-felix.e2d2235e.png"
+  },
+  {
+    "revision": "d98071752d3a94792e86e346523005fe",
+    "url": "/perolas_homer/index.html"
+  }
+];
