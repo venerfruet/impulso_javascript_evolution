1e1be3403674a6ad30e3502e2e37b06488c7df1c
venerfruet <venerfruet@gmail.com>
2022-07-31 20:00:52 -0300

    atualização

--- github-felix/src/components/repository-item/index.js
+++ github-felix/src/components/repository-item/index.js
@@ -6,7 +6,7 @@ const RepositoryItem = ({ name, linkToRepo, fullName }) => {
     <S.Wrapper>
       <S.WrapperTitle>{name}</S.WrapperTitle>
       <S.WrapperFullName>link para o repositório:</S.WrapperFullName>
-      <S.WrapperLink href={`https://github.com/${linkToRepo}`} target="_blank" rel="noreferrer">
+      <S.WrapperLink href={linkToRepo} target="_blank" rel="noreferrer">
         {fullName}
       </S.WrapperLink>
     </S.Wrapper>
